5b2e131514616edbc0f3b51279a87eb477bf1324
fake-user14 <userisfake14@gmail.com>
2025-04-08 23:37:43 +0530

    strings practice

--- com/assignments/java/core/strings.java
+++ com/assignments/java/core/strings.java
@@ -1,22 +1,28 @@
 package com.assignments.java.core;
 
-public class strings {
-
+public class strings{
     public static void main(String[] args) {
-
-        String s="abba";
-        String rev = "";
-        for (int i=s.length()-1; i>=0;i--){
-            rev= rev + s.charAt(i);
+        String s="abhijeet";
+        String reverse=reverseString(s);
+        System.out.println(reverse);
+        if (isPalindrome(s)){
+            System.out.println("yes, palindrome");
         }
-        System.out.println(rev);
-        // how to check if string is a palindrome or not
-        if (s.equals(rev)){
-            System.out.println(" palidrone");
-
-        }else {
-            System.out.println("not palindrone");
+        else
+        {
+            System.out.println("no, not a palindrome");
         }
     }
 
+    public static String reverseString(String s){
+        String rev="";
+        for (int i=s.length()-1;i>=0;i--){
+            rev+=s.charAt(i);
+        }
+        return(rev);
     }
+    public static boolean isPalindrome(String s){
+
+        return s.equals(reverseString(s));
+    }
+}
